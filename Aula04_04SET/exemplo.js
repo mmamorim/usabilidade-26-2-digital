9caf4c966d5f3432fc03f396a30f59872a22a8b7
Marcelo <marc.m.amorim@gmail.com>
2026-09-04 10:42:26 -0300

    v1

---
+++ Aula04_04SET/exemplo.js
@@ -0,0 +1,28 @@
+
+console.log("Aula 4 exemplo");
+
+let frutas = []
+
+function adicionar() {
+    console.log("Chamei o adicionar");
+
+    let elemEntrada = document.getElementById("txtEntrada")
+    let texto = elemEntrada.value
+    //console.log(texto);
+    
+    frutas.push(texto)
+    console.log(frutas)
+
+    let elem = document.getElementById("cxaLista")
+    let bloco = "<ol>"
+    for(let fruta of frutas) {
+        bloco = bloco + "<li>"+fruta+"</li>"
+    }
+    bloco = bloco + "</ol>"
+    console.log(bloco);    
+    elem.innerHTML = bloco
+}
+
+function remover() {
+    
+}
